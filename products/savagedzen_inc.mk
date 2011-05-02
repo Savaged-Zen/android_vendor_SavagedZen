f7b5b823b35e6fee70cd664e9b4af81ad9d23836
Joshua Seidel <jsseidel1@gmail.com>
2011-05-02 06:14:12 -0500

    update build id
    Change-Id: I09995e9714724f47d9a661f90e856b8e60fbd006

--- products/savagedzen_inc.mk
+++ products/savagedzen_inc.mk
@@ -9,7 +9,7 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := inc
 PRODUCT_MODEL := Incredible
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=GRI40 PRODUCT_NAME=inc BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.2/FRF91/264707:user/release-keys PRODUCT_BRAND=verizon_wwe TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="3.26.605.1 CL264707 release-keys" PRODUCT_MODEL=ADR6300 PRODUCT_MANUFACTURER=HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=GRJ22 PRODUCT_NAME=inc BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.2/FRF91/264707:user/release-keys PRODUCT_BRAND=verizon_wwe TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="3.26.605.1 CL264707 release-keys" PRODUCT_MODEL=ADR6300 PRODUCT_MANUFACTURER=HTC
 PRODUCT_PROPERTY_OVERRIDES += ro.product.version=3.26.605.1
 
 
@@ -25,7 +25,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.romversion=SavagedZen(MOD)-$(BUILD_VERSION) \
     ro.modversion=SavagedZen(MOD)-$(BUILD_VERSION) \
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing SavagedZen for your Droid Incredible\nFollow @SZGIT for the latest SavagedZen updates\nGet the latest rom at mirror.savaged-zen.com\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing SavagedZenMOD\nFollow @SZGIT for the latest SavagedZen updates\nGet the latest rom at mirror.savaged-zen.com\n------------------------------------------------\n"
 
 # Extra inc overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/savagedzen/overlay/inc
--- products/savagedzen_supersonic.mk
+++ products/savagedzen_supersonic.mk
@@ -9,7 +9,7 @@ PRODUCT_BRAND := sprint
 PRODUCT_DEVICE := supersonic
 PRODUCT_MODEL := PC36100
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=GRI40 PRODUCT_NAME=htc_supersonic BUILD_FINGERPRINT=sprint/htc_supersonic/supersonic/supersonic:2.2/FRF91/294884:user/release-keys PRIVATE_BUILD_DESC="3.70.651.1 CL294884 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=GRJ22 PRODUCT_NAME=htc_supersonic BUILD_FINGERPRINT=sprint/htc_supersonic/supersonic/supersonic:2.2/FRF91/294884:user/release-keys PRIVATE_BUILD_DESC="3.70.651.1 CL294884 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Evo4G
@@ -23,7 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.romversion=SavagedZen(MOD)-$(BUILD_VERSION) \
     ro.modversion=SavagedZen(MOD)-$(BUILD_VERSION) \
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing SavagedZen(MOD) for your Evo 4G\nPlease visit us at \www.Savaged-Zen.org \nFollow @SZGIT for the latest SavagedZen updates\nGet the latest rom at mirror.savaged-zen.net\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing SavagedZen(MOD)\nPlease visit us at \nwww.Savaged-Zen.org \nFollow @SZGIT for the latest SavagedZen updates\nGet the latest rom at mirror.savaged-zen.net\n------------------------------------------------\n"
 
 # Extra Supersonic overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/savagedzen/overlay/supersonic
