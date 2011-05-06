33e0809fa776f3139d709963c9258e9ceed77322
Joshua Seidel <jsseidel1@gmail.com>
2011-05-06 15:35:29 -0500

    revert the way AndroidProducts selects mks
    Change-Id: Id21372afd44b01ea8164ce479a32f6ae404941d8

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,13 +1,13 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/savagedzen_inc.mk \
-    $(LOCAL_DIR)/savagedzen_supersonic.mk \
-    $(LOCAL_DIR)/savagedzen_glacier.mk
+#PRODUCT_MAKEFILES := \
+#    $(LOCAL_DIR)/savagedzen_inc.mk \
+#    $(LOCAL_DIR)/savagedzen_supersonic.mk \
+#    $(LOCAL_DIR)/savagedzen_glacier.mk
 
 #Temporary work around... * cocide
-#ifeq ($(TARGET_PRODUCT),savagedzen_supersonic)
-#  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_supersonic.mk
-#else ifeq ($(TARGET_PRODUCT),savagedzen_inc)
-#  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_inc.mk
-#else ifeq ($(TARGET_PRODUCT),savagedzen_glacier)
-#  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_glacier.mk
-#endif
+ifeq ($(TARGET_PRODUCT),savagedzen_supersonic)
+  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_supersonic.mk
+else ifeq ($(TARGET_PRODUCT),savagedzen_inc)
+  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_inc.mk
+else ifeq ($(TARGET_PRODUCT),savagedzen_glacier)
+  PRODUCT_MAKEFILES := $(LOCAL_DIR)/savagedzen_glacier.mk
+endif
